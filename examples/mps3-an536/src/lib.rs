9d092f9375c1263bdd52ce9847ef72167a873e72
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2025-10-09 21:33:50 +0100

    Fixup the interrupt table so it's valid Markdown

--- examples/mps3-an536/src/lib.rs
+++ examples/mps3-an536/src/lib.rs
@@ -4,48 +4,48 @@
 //!
 //! | Interrupt ID | Description                  |
 //! |--------------|------------------------------|
-//! | EXTPPI0[0]    | UART 0 Receive Interrupt    |
-//! | EXTPPI0[1]    | UART 0 Transmit Interrupt   |
-//! | EXTPPI0[2]    | UART 0 Combined Interrupt   |
-//! | EXTPPI0[3]    | UART 0 Overflow             |
-//! | EXTPPI1[0]    | UART 1 Receive Interrupt    |
-//! | EXTPPI1[1]    | UART 1 Transmit Interrupt   |
-//! | EXTPPI1[2]    | UART 1 Combined Interrupt   |
-//! | EXTPPI1[3]    | UART 1 Overflow             |
-//! | SP[0]         | WDG                         |
-//! | SP[1]         | DualTimer 1                 |
-//! | SP[2]         | DualTimer 2                 |
-//! | SP[3]         | DualTimer Combined          |
-//! | SP[4]         | RTC                         |
-//! | SP[5]         | UART 2 Receive Interrupt    |
-//! | SP[6]         | UART 2 Transmit Interrupt   |
-//! | SP[7]         | UART 3 Receive Interrupt    |
-//! | SP[8]         | UART 3 Transmit Interrupt   |
-//! | SP[9]         | UART 4 Receive Interrupt    |
-//! | SP[10]        | UART 4 Transmit Interrupt   |
-//! | SP[11]        | UART 5 Receive Interrupt    |
-//! | SP[12]        | UART 5 Transmit Interrupt   |
-//! | SP[13]        | UART 2 Combined Interrupt   |
-//! | SP[14]        | UART 3 Combined Interrupt   |
-//! | SP[15]        | UART 4 Combined Interrupt   |
-//! | SP[16]        | UART 5 Combined Interrupt   |
-//! | SP[17]        | UART Overflow (2, 3, 4 & 5) |
-//! | SP[18]        | Ethernet                    |
-//! | SP[19]        | USB                         |
-//! | SP[20]        | FPGA Audio I2S              |
-//! | SP[21]        | Touch Screen                |
-//! | SP[22]        | SPI ADC                     |
-//! | SP[23]        | SPI Shield 0                |
-//! | SP[24]        | SPI Shield 1                |
-//! | SP[25]        | HDCLCD Interrupt            |
-//! | SP[26]        | GPIO 0 Combined Interrupt   |
-//! | SP[27]        | GPIO 1 Combined Interrupt   |
-//! | SP[28]        | GPIO 2 Combined Interrupt   |
-//! | SP[29]        | GPIO 3 Combined Interrupt   |
-//! | SP[30..=45]   | GPIO 0.x Interrupt          |
-//! | SP[46..=61]   | GPIO 1.x Interrupt          |
-//! | SP[62..=77]   | GPIO 2.x Interrupt          |
-//! | SP[78..=93]   | GPIO 3.x Interrupt          |
+//! | `EXTPPI0[0]` | UART 0 Receive Interrupt    |
+//! | `EXTPPI0[1]` | UART 0 Transmit Interrupt   |
+//! | `EXTPPI0[2]` | UART 0 Combined Interrupt   |
+//! | `EXTPPI0[3]` | UART 0 Overflow             |
+//! | `EXTPPI1[0]` | UART 1 Receive Interrupt    |
+//! | `EXTPPI1[1]` | UART 1 Transmit Interrupt   |
+//! | `EXTPPI1[2]` | UART 1 Combined Interrupt   |
+//! | `EXTPPI1[3]` | UART 1 Overflow             |
+//! | `SP[0]`      | WDG                         |
+//! | `SP[1]`      | DualTimer 1                 |
+//! | `SP[2]`      | DualTimer 2                 |
+//! | `SP[3]`      | DualTimer Combined          |
+//! | `SP[4]`      | RTC                         |
+//! | `SP[5]`      | UART 2 Receive Interrupt    |
+//! | `SP[6]`      | UART 2 Transmit Interrupt   |
+//! | `SP[7]`      | UART 3 Receive Interrupt    |
+//! | `SP[8]`      | UART 3 Transmit Interrupt   |
+//! | `SP[9]`      | UART 4 Receive Interrupt    |
+//! | `SP[10]`     | UART 4 Transmit Interrupt   |
+//! | `SP[11]`     | UART 5 Receive Interrupt    |
+//! | `SP[12]`     | UART 5 Transmit Interrupt   |
+//! | `SP[13]`     | UART 2 Combined Interrupt   |
+//! | `SP[14]`     | UART 3 Combined Interrupt   |
+//! | `SP[15]`     | UART 4 Combined Interrupt   |
+//! | `SP[16]`     | UART 5 Combined Interrupt   |
+//! | `SP[17]`     | UART Overflow (2, 3, 4 & 5) |
+//! | `SP[18]`     | Ethernet                    |
+//! | `SP[19]`     | USB                         |
+//! | `SP[20]`     | FPGA Audio I2S              |
+//! | `SP[21]`     | Touch Screen                |
+//! | `SP[22]`     | SPI ADC                     |
+//! | `SP[23]`     | SPI Shield 0                |
+//! | `SP[24]`     | SPI Shield 1                |
+//! | `SP[25]`     | HDCLCD Interrupt            |
+//! | `SP[26]`     | GPIO 0 Combined Interrupt   |
+//! | `SP[27]`     | GPIO 1 Combined Interrupt   |
+//! | `SP[28]`     | GPIO 2 Combined Interrupt   |
+//! | `SP[29]`     | GPIO 3 Combined Interrupt   |
+//! | `SP[30..=45]`| GPIO 0.x Interrupt          |
+//! | `SP[46..=61]`| GPIO 1.x Interrupt          |
+//! | `SP[62..=77]`| GPIO 2.x Interrupt          |
+//! | `SP[78..=93]`| GPIO 3.x Interrupt          |
 //!
 //! * Interrupt ID `SP[x]` are shared across cores
 //! * Interrupt ID `EXTPPI0[x]` is only available on Core 0
