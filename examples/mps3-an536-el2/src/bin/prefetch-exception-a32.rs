21756ae59862cb5b3d4b396b87b456e308b858c0
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2026-04-15 09:33:57 +0200

    Switch to naked functions for EL2 examples

--- examples/mps3-an536-el2/src/bin/prefetch-exception-a32.rs
+++ examples/mps3-an536-el2/src/bin/prefetch-exception-a32.rs
@@ -19,33 +19,25 @@ fn main() -> ! {
 
     // A BKPT instruction triggers a Prefetch Abort except when Halting debug-mode is enabled.
     // See p. 2038 of ARMv7-M Architecture Reference Manual
-    unsafe {
-        // trigger an prefetch abort exception, from A32 (Arm) mode
-        bkpt_from_a32();
-    }
+
+    // trigger an prefetch abort exception, from A32 (Arm) mode
+    bkpt_from_a32();
 
     println!("Recovered from fault OK!");
 
     mps3_an536_el2::exit(0);
 }
 
-// These functions are written in assembly
-unsafe extern "C" {
-    fn bkpt_from_a32();
-}
-
-core::arch::global_asm!(
-    r#"
-    // fn bkpt_from_a32();
-    .arm
-    .global bkpt_from_a32
-    .type bkpt_from_a32, %function
-    bkpt_from_a32:
+#[unsafe(naked)]
+#[instruction_set(arm::a32)]
+extern "C" fn bkpt_from_a32() {
+    core::arch::naked_asm!(
+        r#"
         bkpt    #0
         bx      lr
-    .size bkpt_from_a32, . - bkpt_from_a32
-"#
-);
+        "#
+    );
+}
 
 // Custom link sections are allowed as well.
 #[exception(Undefined)]
@@ -58,12 +50,12 @@ unsafe fn prefetch_abort_handler(addr: usize) -> usize {
     let hsr = aarch32_cpu::register::Hsr::read();
     println!("prefetch abort occurred {:08x?}, {:x?}", hsr, hsr.get_iss());
 
-    if addr == bkpt_from_a32 as unsafe extern "C" fn() as usize {
+    if addr == bkpt_from_a32 as extern "C" fn() as usize {
         println!("caught bkpt_from_a32");
     } else {
         println!(
             "Bad fault address {:08x} is not {:08x}",
-            addr, bkpt_from_a32 as unsafe extern "C" fn() as usize
+            addr, bkpt_from_a32 as extern "C" fn() as usize
         );
     }
 
